092f6d023d00530a019f5b80d438d59550b63727
tsparrot <1134879109@qq.com>
2019-10-18 00:07:44 +0800

    Final

--- tsParrotCage/settings.py
+++ tsParrotCage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&$_2wl3&tu)81u+z7md1sfr7%e!w$umm@mv0(lgs1iggks-bsn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
